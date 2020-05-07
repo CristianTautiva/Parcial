93f6d2eed8ae665bc668b9773138a35a3be09d75
CristianTautiva <cristianricardotl@ufps.edu.co>
2020-05-07 06:56:24 -0500

    Primer commit

---
+++ js/funcionalidad.js
@@ -0,0 +1,19 @@
+
+$("#quienes_somos").click(function(event){
+    $("#contenedor").load("quienes_somos/form.html");
+});
+
+$("#registrarme").click(function(event){
+    $("#contenedor").load("registro/form.html");
+});
+
+$("#iniciar_sesion").click(function(event){
+  $("#contenedor").load("iniciar_sesion/form.html");
+});
+$("#productos").click(function(event){
+  $("#contenedor").load("productos/form.html");
+});
+
+
+
+
